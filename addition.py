5bc72581908ba1293f43688d8de35973d671571c
Narayanan Arvind <64064488+arviinnd-5989@users.noreply.github.com>
2023-11-01 17:17:08 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,11 @@
+''' Goal: Addition of two numbers
+Title: Addition
+Details: Addition of two numbers using Jenkins and Git
+Author: @N.Arvind
+Date: 01-Nov-2023
+'''
+
+def sum(x,y):
+  return x+y
+
+print(sum(2,3))
